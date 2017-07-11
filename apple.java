f9de0b9f3c8e01f2a40d4a717fbb2f467afdd7b2
Naresh <Pulanareshroyal@gmail.com>
2017-07-10 22:26:46 -0700

    i commited by something adding in apple.java

--- apple.java
+++ apple.java
@@ -2,5 +2,6 @@ this is the file to taken the no. of values in the particular file.
 they can easy to access to maintain to the file.
 i am acceptable for the plase.
 now i am maintained by the file.
-ASD =523.
+ASD =Naresh520.
 this is the value.
+and also take this value also.
